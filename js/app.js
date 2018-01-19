28d44421b97c12c04cbf7b58075965f0d9253edb
Costas Zeimpekis <morpheustudio@gmail.com>
2018-01-19 17:08:46 +0200

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+document.activeElement
+
+document.addEventListener('click',goWilds)
+
+function goWilds(){
+  console.log('Hello');
+}
+
+document.addEventListener('click',goWilds)
+
+function goWilds(){
+function goWilds(){
+  console.log('Hello');
+}
+
+
+ 
